90258b9be4347b8452c79b2757ac7b0af52e08e7
kason-huang <qinkai1994@qq.com>
2025-08-15 06:29:19 +0000

    update

---
+++ run_tensorboard.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+tensorboard --logdir  ./tb_0814 --host 0.0.0.0 --port 1234
---
+++ train_goat.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python -m ovon.run \
+  --run-type train \
+  --exp-config config/experiments/transformer_rl_goat.yaml
+
+
