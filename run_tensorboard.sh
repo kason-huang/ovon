0664d3ad4730e78362a81abc98748412b2b2bed2
kason-huang <qinkai1994@qq.com>
2025-08-18 15:42:13 +0800

    update for 4v100

--- run_tensorboard.sh
+++ run_tensorboard.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-tensorboard --logdir  ./tb_0814 --host 0.0.0.0 --port 1234
+
+# 检查是否传入参数
+if [ $# -lt 1 ]; then
+  echo "用法: $0 <logdir>"
+  exit 1
+fi
+
+LOGDIR=$1
+
+tensorboard --logdir "$LOGDIR" --host 0.0.0.0 --port 1234
---
+++ train_goat_4_v100.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#python -m ovon.run \
+#  --run-type train \
+#  --exp-config config/experiments/transformer_rl_goat.yaml
+export OMP_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+torchrun --standalone --nproc_per_node=4 \
+	 -m ovon.run \
+	 --run-type train \
+	 --exp-config config/experiments/transformer_rl_goat_4v100.yaml \
+
+
